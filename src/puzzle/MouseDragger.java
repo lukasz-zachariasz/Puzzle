abd9cf69b19a68daaa3b803598cd89dceee1e69a
lukasz-zachariasz <lukas.zachi@gmail.com>
2015-06-01 13:56:28 +0200

    gotowe nie dopieszczone

--- src/puzzle/MouseDragger.java
+++ src/puzzle/MouseDragger.java
@@ -9,6 +9,10 @@ import java.awt.Component;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 /**
@@ -18,20 +22,24 @@ import javax.swing.SwingUtilities;
 public class MouseDragger extends MouseAdapter {
         private Point lastLocation;
         private Component draggedComponent;
+        private Point newLocation;
+        private Point location;
+        public Point prevLocation;
 
         //wciśnięcie klawisza myszki
         @Override
         public void mousePressed(MouseEvent e) {
             draggedComponent = e.getComponent();
             lastLocation = SwingUtilities.convertPoint(draggedComponent, e.getPoint(), draggedComponent.getParent());
+            prevLocation = lastLocation;
         }
 
         //przesuwanie wciśniętego klawisza myszki
         @Override
         public void mouseDragged(MouseEvent e) {
-            Point location = SwingUtilities.convertPoint(draggedComponent, e.getPoint(), draggedComponent.getParent());
+            location = SwingUtilities.convertPoint(draggedComponent, e.getPoint(), draggedComponent.getParent());
             if (draggedComponent.getParent().getBounds().contains(location)) {
-                Point newLocation = draggedComponent.getLocation();
+                newLocation = draggedComponent.getLocation();
                 newLocation.translate(location.x - lastLocation.x, location.y - lastLocation.y);
                 newLocation.x = Math.max(newLocation.x, 0);
                 newLocation.x = Math.min(newLocation.x, 1000 - draggedComponent.getWidth());
@@ -45,6 +53,34 @@ public class MouseDragger extends MouseAdapter {
         //puszczenie klawisza myszki
         @Override
         public void mouseReleased(MouseEvent e) {
+            draggedComponent.setLocation(5000,5000);
+            
+            Component dragComp2 = Puzzle.contentPane.findComponentAt((newLocation.x+100)-(newLocation.x+100)%200, (newLocation.y+50)-(newLocation.y+50)%100);
+            if(dragComp2 instanceof JLabel) dragComp2.setLocation((prevLocation.x)-(prevLocation.x)%200, (prevLocation.y)-(prevLocation.y)%100);
+            draggedComponent.setLocation((newLocation.x+100)-(newLocation.x+100)%200, (newLocation.y+50)-(newLocation.y+50)%100);
+            int tx;
+            int ty;
+            tx=(newLocation.x+100)/200;
+            if(tx>4)tx=4;
+            ty=(newLocation.y+50)/100;
+            if(ty>4)ty=4;
+            
+            int temp = Puzzle.gamearray[prevLocation.x/200][prevLocation.y/100];
+            Puzzle.gamearray[prevLocation.x/200][prevLocation.y/100] = Puzzle.gamearray[tx][ty];
+            Puzzle.gamearray[tx][ty] = temp;
+            try {
+                Puzzle.check();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(MouseDragger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.println(Puzzle.gamearray[i][j]);
+
+        }
+    }
             lastLocation = null;
             draggedComponent = null;
         }
--- src/puzzle/Puzzle.java
+++ src/puzzle/Puzzle.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.util.Random;
 import java.util.Scanner;
@@ -24,18 +25,66 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Puzzle extends JFrame {
 
-    private static JLayeredPane contentPane;
-    public int[][] gamearray;
+    public static JLayeredPane contentPane;
+    public static int[][] gamearray;
+    private static Long czas;
+
+    static int check() throws FileNotFoundException {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(!(gamearray[i][j]==i*5+j))
+                {
+                    return 0;
+                }
+        }
+    }
+                czas=System.currentTimeMillis()-czas;
+                
+                                File file = new File("rank.txt");
+                                Scanner in = new Scanner(file);
+                                long tab[] = new long[3];
+                                long temp=0;
+                                tab[0]=in.nextLong();
+                                tab[1]=in.nextLong();
+                                tab[2]=in.nextLong();
+                                in.close();
+                                if(czas>tab[0]&&czas>tab[1]&&czas<tab[2])
+                                {
+                                    tab[2]=czas;
+                                }
+                                if(czas>tab[0]&&czas<tab[1]){
+                                    tab[2]=tab[1];
+                                    tab[1]=czas;
+                                }
+                                if(czas<tab[0]){
+                                    temp=tab[0];
+                                    tab[0]=czas;
+                                    tab[2]=tab[1];
+                                    tab[1]=temp;
+                                }
+                
+                 PrintWriter zapis = new PrintWriter("rank.txt");
+                    zapis.println(tab[0]);
+                    zapis.println(tab[1]);
+                    zapis.println(tab[2]);
+                    zapis.close();
+                    System.exit(0);
+        return 0;
+    }
+    
     private File selectedFile;
     private JButton jButton1;
     private JButton jButton2;
     private JButton jButton3;
-    private JFrame frame;
+    public static JFrame frame;
     private JButton jButton4;
     private JLabel jLabel1;
     private JLabel jLabel2;
     private JLabel jLabel3;
     
+        
         private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         System.exit(0);
     }  
@@ -46,8 +95,6 @@ public class Puzzle extends JFrame {
         jButton1.setVisible(false);
         jButton2.setVisible(false);
         jButton3.setVisible(false);
-        
-        //okienko wczytywania
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         String[] EXTENSION=new String[]{"jpeg","jpg","png","bmp"};
@@ -56,6 +103,7 @@ public class Puzzle extends JFrame {
         int result = fileChooser.showOpenDialog(frame);
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
+           czas = System.currentTimeMillis();
         }
         else if (result == JFileChooser.CANCEL_OPTION) {
             System.exit(0);
@@ -96,7 +144,7 @@ public class Puzzle extends JFrame {
                     rj=random.nextInt(5);
                     if(gamearray[ri][rj]==-1)
                     {
-                        gamearray[ri][rj]=i*5+j;
+                        gamearray[ri][rj]=(i-1)*5+(j-1);
                         break;
                     }
                 }
@@ -106,9 +154,10 @@ public class Puzzle extends JFrame {
                 draggableImage.setLocation((ri)*200, (rj)*100);                                                                    
                 dragger.makeDraggable(draggableImage);
                 contentPane.add(draggableImage);
+                //contentPane.getComponentZOrder(contentPane.add(draggableImage));
             }
-        }
     }
+        }
 
     protected void initUI() throws MalformedURLException, IOException {
         //załadowanie okna gry
@@ -157,15 +206,13 @@ public class Puzzle extends JFrame {
                 }
             }
 
-            //wczytanie rankingu
             private void jButton2ActionPerformed(ActionEvent evt) throws FileNotFoundException {
 
                 File file = new File("rank.txt");
                 Scanner in = new Scanner(file);
- 
-                jLabel1.setText(in.nextLine());
-                jLabel2.setText(in.nextLine());
-                jLabel3.setText(in.nextLine());
+                jLabel1.setText("1."+in.nextLine());
+                jLabel2.setText("2."+in.nextLine());
+                jLabel3.setText("3."+in.nextLine());
                 jButton1.setVisible(false);
                 jButton2.setVisible(false);
                 jButton3.setVisible(false);
@@ -233,70 +280,9 @@ public class Puzzle extends JFrame {
                 .addComponent(jButton4)
                 .addContainerGap(230, Short.MAX_VALUE))
         );
-
+        
         
         frame.setVisible(true);          
-        
-/*
-        
-        //wczytywanie plików
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-        String[] EXTENSION=new String[]{"jpeg","jpg","png","bmp"};
-        FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter("Pliki obrazów", EXTENSION);
-            fileChooser.setFileFilter(xmlfilter);
-        int result = fileChooser.showOpenDialog(frame);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            selectedFile = fileChooser.getSelectedFile();
-        }
-        else if (result == JFileChooser.CANCEL_OPTION) {
-            System.exit(0);
-        }
-        
-        //wczytanie obrazu z wybranego pliku
-        BufferedImage image = new BufferedImage(1000, 500, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = image.createGraphics();
-        g.drawImage(ImageIO.read(selectedFile), 0, 0, 1000, 500, null);
-        g.dispose();
-       MouseDragger dragger = new MouseDragger();
-        Random random = new Random();
-        BufferedImage temp;
-        
-        //Win conditionsy kurwa mać
-        
-        //przetasowanie kawałków
-        int[][] gamearray = new int[5][5];
-        int ri;
-        int rj;
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                gamearray[i][j]=-1;
-            }
-        }
-        
-        //pocięcie wybranego obrazu i wyświetlenie w tablicy przetasowań
-        for(int i = 1; i <= 5; i++)
-        {
-            for(int j = 1; j <= 5; j++)
-            {
-                for(;;)
-                {
-                    ri=random.nextInt(5);
-                    rj=random.nextInt(5);
-                    if(gamearray[ri][rj]==-1)
-                    {
-                        gamearray[ri][rj]=i*5+j;
-                        break;
-                    }
-                }
-                temp = image.getSubimage((i-1)*200, (j-1)*100, 200, 100);
-                JLabel draggableImage = new JLabel(new ImageIcon(temp));
-                draggableImage.setSize(draggableImage.getPreferredSize());
-                draggableImage.setLocation((ri)*200, (rj)*100);                                                                    
-                dragger.makeDraggable(draggableImage);
-                contentPane.add(draggableImage);
-            }
-        }*/
         contentPane.repaint();
     }
 
