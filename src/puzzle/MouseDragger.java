e195ffd57ad12394df8381fde3bb22c2986328be
Skenior <Skenior@Skenior-GT>
2015-06-11 20:34:29 +0200

    Spacje :D

--- src/puzzle/MouseDragger.java
+++ src/puzzle/MouseDragger.java
@@ -55,27 +55,27 @@ public class MouseDragger extends MouseAdapter {
         public void mouseReleased(MouseEvent e) {
             draggedComponent.setLocation(5000,5000);
             
-            Component dragComp2 = Puzzle.contentPane.findComponentAt((newLocation.x+100)-(newLocation.x+100)%200, (newLocation.y+50)-(newLocation.y+50)%100);
-            if(dragComp2 instanceof JLabel) dragComp2.setLocation((prevLocation.x)-(prevLocation.x)%200, (prevLocation.y)-(prevLocation.y)%100);
-            draggedComponent.setLocation((newLocation.x+100)-(newLocation.x+100)%200, (newLocation.y+50)-(newLocation.y+50)%100);
+            Component dragComp2 = Puzzle.contentPane.findComponentAt((newLocation.x + 100)-(newLocation.x + 100) % 200, (newLocation.y + 50)-(newLocation.y + 50) % 100);
+            if(dragComp2 instanceof JLabel) dragComp2.setLocation((prevLocation.x) - (prevLocation.x)%200, (prevLocation.y)-(prevLocation.y) % 100);
+            draggedComponent.setLocation((newLocation.x + 100)-(newLocation.x + 100) % 200, (newLocation.y + 50)-(newLocation.y + 50)%100);
             int tx;
             int ty;
-            tx=(newLocation.x+100)/200;
-            if(tx>4)tx=4;
-            ty=(newLocation.y+50)/100;
-            if(ty>4)ty=4;
+            tx=(newLocation.x + 100) / 200;
+            if(tx > 4)tx = 4;
+            ty=(newLocation.y + 50) / 100;
+            if(ty > 4)ty = 4;
             
-            int temp = Puzzle.gamearray[prevLocation.x/200][prevLocation.y/100];
-            Puzzle.gamearray[prevLocation.x/200][prevLocation.y/100] = Puzzle.gamearray[tx][ty];
+            int temp = Puzzle.gamearray[prevLocation.x / 200][prevLocation.y / 100];
+            Puzzle.gamearray[prevLocation.x / 200][prevLocation.y / 100] = Puzzle.gamearray[tx][ty];
             Puzzle.gamearray[tx][ty] = temp;
             try {
                 Puzzle.check();
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(MouseDragger.class.getName()).log(Level.SEVERE, null, ex);
             }
-            for(int i=0;i<5;i++)
+            for(int i = 0; i < 5; i++)
         {
-            for(int j=0;j<5;j++)
+            for(int j = 0;j < 5 ; j++)
             {
                 System.out.println(Puzzle.gamearray[i][j]);
 
